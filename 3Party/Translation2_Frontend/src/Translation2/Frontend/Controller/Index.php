8cd637352a7fefee98328188f82afbf3755bf8eb
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2009-04-07 07:40:24 +0000

    Updated functionality

--- 3Party/Translation2_Frontend/src/Translation2/Frontend/Controller/Index.php
+++ 3Party/Translation2_Frontend/src/Translation2/Frontend/Controller/Index.php
@@ -1,17 +1,34 @@
 <?php
 class Translation2_Frontend_Controller_Index extends k_Controller
 {
-    public $map = array('create' => 'Translation2_Frontend_Controller_Create',
-                        'language' => 'Translation2_Frontend_Controller_Lang_Index');
+    
+    private $message = array();
+    
+    public $map = array('language' => 'Translation2_Frontend_Controller_Lang_Index',
+                        'search' => 'Translation2_Frontend_Controller_Search');
 
-    function GET()
+    function GET($values = array())
     {
+        
         $this->document->title = 'Translation';
         $this->document->options = array(
-            $this->url('language') => 'Add language'
+            $this->url('language') => 'Add language',
+            $this->url('search') => 'Search for translation'
         );
         
+        if(!empty($this->GET['edit_id']) && !empty($this->GET['page_id'])) {
+            $values = array();
+            $values['identifier'] = $this->GET['edit_id'];
+            $values['page_id'] = $this->GET['page_id'];
+            foreach($this->getLangs() AS $lang => $description) {
+                $values['translation'][$lang] = $this->getTranslation()->get($this->GET['edit_id'], $this->GET['page_id'], $lang);
+            }
+        }
+        
         $data['created_page_id'] = $this->getCreatedPageId();
+        $data['message'] = $this->getMessages();
+        $data['langs'] = $this->getLangs();
+        $data['values'] = $values;
         
         $content = $this->render('Translation2/Frontend/templates/create-tpl.php', $data);
         return $this->render('Translation2/Frontend/templates/wrapper-tpl.php', $data = array('content' => $content));
@@ -20,23 +37,65 @@ class Translation2_Frontend_Controller_Index extends k_Controller
     function POST()
     {
         
-        if(isset($this->POST['new_page_id'])) {
+        if(!empty($this->POST['new_page_id'])) {
             $page_id = $this->POST['new_page_id'];
-        } elseif($this->POST['page_id'] == '<none>') {
-            $page_id = null;
-        } else {
+        } elseif(!empty($this->POST['page_id']) && $this->POST['page_id'] != '<none>') {
             $page_id = $this->POST['page_id'];
+        } else {
+            $this->setMessage('You need to provide a pageID');
+            return $this->GET();
         }
         
-        $result = $this->getTranslationAdmin()->add($this->POST['identifier'], $page_id, $this->POST['translation']);
+        if(empty($this->POST['identifier'])) {
+            $this->setMessage('You need to fill in the identifier');
+            return $this->GET();
+        }
+        
+        $identifier = $this->POST['identifier'];
+        
+        $page = $this->getTranslation()->getPage($page_id);
+        if(empty($this->POST['overwrite']) && isset($page[$identifier])) {
+            $this->setMessage('The translation does already exist');
+            foreach($this->getLangs() AS $lang => $description) {
+                $this->setMessage(ucfirst($description).': '.$this->getTranslation()->get($identifier, $page_id, $lang), 'translation_'.$lang);
+            }
+            $values = $this->POST->getArrayCopy();
+            $values['overwrite'] = 1;
+            return $this->GET($values);
+        }
+        
+        if(false !== ($common_page_id = $this->getCommonPageId())) {
+            $page = $this->getTranslation()->getPage($common_page_id);
+            if(empty($this->POST['overwrite']) && isset($page[$identifier])) {
+                $this->setMessage('The translation does already exist in common');
+                foreach($this->getLangs() AS $lang => $description) {
+                    $this->setMessage(ucfirst($description).': '.$this->getTranslation()->get($identifier, $common_page_id, $lang), 'translation_'.$lang);
+                }
+                $values = $this->POST->getArrayCopy();
+                $values['overwrite'] = 1;
+                return $this->GET($values);
+            }
+        }
+        
+        $result = $this->getTranslationAdmin()->add($identifier, $page_id, $this->POST['translation']);
         if (PEAR::isError($result)) {
             throw new Exception($result->getMessage());
         }
         
+        $message = 'Translation successfully added!<br />Identifier: '.$identifier.'<br />PageID: '.$page_id;
+        foreach($this->getLangs() AS $lang => $description) {
+            $message .= '<br />'.ucfirst($description).': '.$this->POST['translation'][$lang];
+        }
+        $this->setMessage($message);
         
-        
+        return $this->GET();
     }
-
+    
+    function getLangs()
+    {
+        return $this->getTranslationAdmin()->getLangs();
+    }
+    
     function getTranslationAdmin()
     {
         return $this->registry->get('translation_admin');
@@ -44,7 +103,9 @@ class Translation2_Frontend_Controller_Index extends k_Controller
 
     function getTranslation()
     {
-        return $this->registry->get('translation'); 
+        $translation = $this->registry->get('translation');
+        $translation = $translation->getDecorator('DefaultText');
+        return $translation;
     }
     
     function getCreatedPageId()
@@ -65,6 +126,25 @@ class Translation2_Frontend_Controller_Index extends k_Controller
         $content = $next->handleRequest();
         return $this->render(dirname(__FILE__) . '/../templates/wrapper-tpl.php', $data = array('content' => $content));
         
+    }
+    
+    private function setMessage($message, $place = 'main')
+    {
+        $this->message[$place] = $message;
+    }
+    
+    private function getMessages()
+    {
+        return $this->message;
+    }
+    
+    public function getCommonPageId()
+    {
+        if(is_callable(array($this->context, 'getTranslationCommonPageId'))) {
+            return $this->context->getTranslationCommonPageId();
+        }
+        
+        return false;
     }
 }
 
--- 3Party/Translation2_Frontend/src/Translation2/Frontend/Controller/Search.php
+++ 3Party/Translation2_Frontend/src/Translation2/Frontend/Controller/Search.php
@@ -1,10 +1,31 @@
 <?php
-class PEAR_Translation2_Controller_Search extends k_Controller
+class Translation2_Frontend_Controller_Search extends k_Controller
 {
     function GET()
     {
-        $db = $this->registry->get('db_sql');
-        return $this->render(dirname(__FILE__) . '/../tpl/search-tpl.php', array('db' => $db));
+        $this->document->title = 'Search';
+        $this->document->options = array(
+            $this->url('../') => 'Back'
+        );
+        
+        
+        $data['result'] = array();
+        
+        if(!empty($this->GET['search'])) {
+            $sql = 'SELECT * FROM core_translation_i18n WHERE id LIKE "%'.$this->GET['search'].'%"';
+            foreach($this->context->getLangs() AS $lang => $description) {
+                $sql .= ' OR '.$lang.' LIKE "%'.$this->GET['search'].'%"';
+            }
+            $mdb2 = $this->registry->get('mdb2');
+            $result = $mdb2->query($sql);
+            if(PEAR::isError($result)) {
+                throw new Exception('Error in query', $result->getUserInfo());
+            }
+            $data['result'] = $result->fetchAll();
+        }
+        
+        $data['langs'] = $this->context->getLangs();
+        return $this->render('Translation2/Frontend/templates/search-tpl.php', $data);
     }
 
 }
